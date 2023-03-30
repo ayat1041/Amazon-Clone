8caf25129ddc5cda42e16d752640b830cb39c638
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-03-30 22:23:31 +0600

    still not gone mad

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -15,9 +15,28 @@ const Shop = () => {
   }, []);
   //just printing the cart obj to see whats there
   useEffect(() => {
+    //it wont show the products as products.json might not be yet loaded and this useeffect has already been executed
+    //thats why we set dependency
+    //though first time it will run by default and 2nd time it will run whenever products value changes
+    // console.log('products',products);
     const storedCart = getShoppingCart();
-    console.log(storedCart);
-  }, []);
+    // console.log("stored cart",storedCart);
+    // step 1 : get id
+    for(const id in storedCart){
+        // console.log(id)
+        //step 2 : get the product using id ; we used find as its obvious that only one unique id will be there
+        const addedProduct = products.find(product => product.id === id);
+        // console.log(addedProduct);
+        // step 3: get quantity of the product
+        // for dependency first addedProduct is empty
+        if(addedProduct){
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        console.log(addedProduct);
+        }
+        
+    }
+  }, [products]);
   // declaring a function that takes a obj and adds that obj to newCart variable along with existing cart objects and updates localStorage
   const handleAddToCart = (product) => {
     // cart.push(product) //but it doesnt work in react as state is immutable
