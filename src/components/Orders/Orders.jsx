00033b6b92baa2432ec963c08295f98ec563d7fd
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-04-05 12:21:11 +0600

    done products loader in orders

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import Cart from '../Cart/Cart';
+import { useLoaderData } from 'react-router-dom';
 
 const Orders = () => {
+    const products = useLoaderData();
+    console.log(products);
     return (
-        <div>
-            <h2>Orders page</h2>
+        <div className='shop-container'>
+            <div className='products-container'>
+                <h2>Orders page: {products.length}</h2>
+                {/* <h2>Orders page: {products.length}</h2> */}
+            </div>
+            <div className='cart-container'>
+                <Cart cart={[]}></Cart>
+            </div>
         </div>
     );
 };
 
-export default Orders;<h2>Orders page</h2>
+export default Orders;
