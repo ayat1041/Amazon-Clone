a3fe2007b5b546884f61df951c3f1d53d27e3883
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-04-05 12:42:31 +0600

    get cart products with  quantities

--- src/loaders/cartProductsLoader.js
+++ src/loaders/cartProductsLoader.js
@@ -1,8 +1,26 @@
+import { getShoppingCart } from "../utilities/fakedb";
+
 const cartProductsLoader = async() => {
     const loadedProducts = await fetch('products.json');
     const products = await loadedProducts.json();
-    console.log(products);
-    return products;
+    
+    // if cart data is in database u must use async await
+
+    const storedCart = getShoppingCart();
+    const savedCart = [];
+    // {9b169efb-0966-40f3-b9c2-79b16cb96388: 1, 5db3ee2f-2eb7-4764-942d-894f72198e86: 1}
+    console.log(storedCart);
+    for(const id in storedCart){
+        const addedProduct = products.find(pd => pd.id === id);
+        if(addedProduct){
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            savedCart.push(addedProduct);
+        }
+    }
+    // console.log(products);
+    return savedCart;
+    // 2
 }
 
 // export {cartProductsLoader};
