30fa1c6d5156b5285613c95744213c5ff9a7988e
Ayat Rahman <57280365+ayat1041@users.noreply.github.com>
2023-04-30 14:54:42 +0600

    upto private route

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,8 +5,13 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../providers/AuthProvider";
 
 function Header() {
-  const { user } = useContext(AuthContext);
+  const { user,logOut } = useContext(AuthContext);
   console.log(user);
+  const handleLogOut = () =>{
+    logOut()
+    .then(result => {})
+    .catch(e =>console.log(e))
+  }
   return (
     <nav className="header">
       <img src={logo} alt="logo" />
@@ -16,7 +21,7 @@ function Header() {
         <Link to="/inventory">Inventory</Link>
         <Link to="/login">Login</Link>
         <Link to="/signup">Sign up</Link>
-        {user && <span style={{color: 'white'}}>Welcome {user.email}</span>}
+        {user && <span style={{color: 'white',marginLeft: "8px"}}>Welcome {user.email} <button style={{color: "white", backgroundColor: "orange", padding: "8px"}} onClick={handleLogOut}>Sign Out</button></span>}
       </div>
     </nav>
   );
